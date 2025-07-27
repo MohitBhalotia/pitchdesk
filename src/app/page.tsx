3b945ea292cfb2b8edaabe221dfdbaa8d2c63618
Mohit Bhalotia <158545409+MohitBhalotia@users.noreply.github.com>
2025-07-28 00:42:39 +0530

    Merged auth branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import Footer4Col from "@/components/mvpblocks/footer-4col";
 import Header1 from "@/components/mvpblocks/header-1";
 import SimplePricing from "@/components/mvpblocks/simple-pricing";
 import TestimonialsCarousel from "@/components/mvpblocks/testimonials-carousel";
-import Image from "next/image";
 
 export default function Home() {
   return (
