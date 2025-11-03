c5360ade1ea776af9f84c6d78f3f559b4f459a9e
Mohit Bhalotia <158545409+MohitBhalotia@users.noreply.github.com>
2025-11-03 17:46:03 +0530

    fix lint

--- src/app/(others)/payment/page.tsx
+++ src/app/(others)/payment/page.tsx
@@ -313,6 +313,7 @@ export default function PricingSection() {
       },
     };
 
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const rzp = new (window as any).Razorpay(options);
     rzp.open();
   } catch (err) {
