9aeac6acfb5b9095b1395d2a61a477aa35fddfce
Mohit Bhalotia <158545409+MohitBhalotia@users.noreply.github.com>
2025-12-29 16:26:34 +0530

    add email campaigns

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,11 +1,12 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
 import { updatePitch, sendEmail } from "@/lib/inngest/functions";
-
+import { sendPracticeEmail } from "@/lib/inngest/send-practice-email";
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     updatePitch,
     sendEmail,
+    sendPracticeEmail,
   ],
 });
