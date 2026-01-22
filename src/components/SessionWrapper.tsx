febb784192a4a0019dac47940405a862467eaed4
Mohit Bhalotia <158545409+MohitBhalotia@users.noreply.github.com>
2026-01-23 02:54:11 +0530

    add tricolour snow

--- src/components/SessionWrapper.tsx
+++ src/components/SessionWrapper.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useIsMobile } from "@/hooks/use-mobile";
 import { SessionProvider } from "next-auth/react";
 import Snowfall from "react-snowfall";
 
@@ -8,11 +9,12 @@ export default function SessionWrapper({
 }: {
   children: React.ReactNode;
 }) {
+  const isMobile = useIsMobile();
   return (
     <SessionProvider>
-      <Snowfall color="orange" snowflakeCount={100} wind={[-0.5, 0.5]} style={{position: 'fixed'}} />
-      <Snowfall color="green" snowflakeCount={100} wind={[-0.5, 0.5]} style={{position: 'fixed'}} />
-      <Snowfall snowflakeCount={100} wind={[-0.5, 0.5]} style={{position: 'fixed'}} />
+      <Snowfall color="orange" snowflakeCount={isMobile ? 100 : 50} wind={[-0.5, 0.5]} style={{position: 'fixed'}} />
+      <Snowfall color="green" snowflakeCount={isMobile ? 100 : 50} wind={[-0.5, 0.5]} style={{position: 'fixed'}} />
+      <Snowfall snowflakeCount={isMobile ? 100 : 50} wind={[-0.5, 0.5]} style={{position: 'fixed'}} />
       {children}
     </SessionProvider>
   );
